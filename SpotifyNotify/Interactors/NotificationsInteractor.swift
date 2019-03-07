70268ab9cf4e594971eba9491c2f7e66620f08a7
Paul Williamson <squarefrog@gmail.com>
2019-03-07 10:21:56 +0000

    Create NotificationViewModel
    This will reduce duplicated code when using two different Notification
    frameworks

--- SpotifyNotify/Interactors/NotificationsInteractor.swift
+++ SpotifyNotify/Interactors/NotificationsInteractor.swift
@@ -35,98 +35,72 @@ final class NotificationsInteractor {
 		guard spotifyInteractor.playerState == .some(.playing) else {
 			return
 		}
-        
-		let notification = NSUserNotification()
-		
-		// decide whether to add progress
-        if preferences.showSongProgress {
-            let artist = currentTrack?.artist ?? "______"
-            let album = currentTrack?.album ?? "______"
-            let duration = progress(for: currentTrack)
-            
-            notification.title = currentTrack?.name
-            notification.subtitle = "\(artist) - \(album)"
-            notification.informativeText = duration
-        } else {
-            notification.title = currentTrack?.name
-            notification.subtitle = currentTrack?.artist
-            notification.informativeText = currentTrack?.album
-        }
-        
-		notification.hasActionButton = true
-		notification.actionButtonTitle = "Skip"
-		
-		// decide whether to add art
-		if SystemPreferences.isContentImagePropertyAvailable && preferences.showAlbumArt {
-			currentTrack?.artworkURL?.asyncImage { art in
-				
-				// decide whether to add spotify icon
-				if self.preferences.showSpotifyIcon {
-					notification.contentImage = art
-				} else {
-					notification.identityImage = art
-					
-					// decide whether to round art
-					if self.preferences.roundAlbumArt {
-						notification.identityImageStyle = .rounded
-					} else {
-						notification.identityImageStyle = .normal
-					}
-				}
-				
-				// remove previous notification and replace it with one with image
-				DispatchQueue.main.async {
-					NSUserNotificationCenter.default.removeAllDeliveredNotifications()
-					NSUserNotificationCenter.default.deliver(notification)
-				}
-			}
-		}
-		
-		// decide whether to add sound
-		if preferences.notificationsSound {
-			notification.soundName = NSUserNotificationDefaultSoundName
-		}
-		
-        NSUserNotificationCenter.default.removeAllDeliveredNotifications()
-		NSUserNotificationCenter.default.deliver(notification)
-		
-		// remove after userset number of seconds if not taken action
-		DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(preferences.notificationsLength)) {
-			NSUserNotificationCenter.default.removeDeliveredNotification(notification)
-		}
+
+        /// return if current track is nil
+        guard let currentTrack = currentTrack else { return }
+
+        let viewModel = NotificationViewModel(track: currentTrack)
+        createLegacyNotification(using: viewModel)
 	}
-	
+
+    /// Use `NSUserNotificationCenter` to deliver the notification in macOS 10.13 and below
+    private func createLegacyNotification(using viewModel: NotificationViewModel) {
+        let notification = NSUserNotification()
+
+        notification.identifier = viewModel.identifier
+        notification.title = viewModel.title
+        notification.subtitle = viewModel.subtitle
+        notification.informativeText = viewModel.body
+        notification.hasActionButton = true
+        notification.actionButtonTitle = "Skip"
+
+        addArtwork(to: notification, using: viewModel)
+
+        // decide whether to add sound
+        if preferences.notificationsSound {
+            notification.soundName = NSUserNotificationDefaultSoundName
+        }
+
+        deliverLegacyNotification(notification)
+    }
+
+    private func addArtwork(to notification: NSUserNotification, using viewModel: NotificationViewModel) {
+        guard viewModel.shouldShowArtwork else { return }
+
+        viewModel.artworkURL?.asyncImage { art in
+            // decide whether to add spotify icon
+            if self.preferences.showSpotifyIcon {
+                notification.contentImage = art
+            } else {
+                notification.identityImage = art
+
+                // decide whether to round art
+                if self.preferences.roundAlbumArt {
+                    notification.identityImageStyle = .rounded
+                } else {
+                    notification.identityImageStyle = .normal
+                }
+            }
+
+            // remove previous notification and replace it with one with image
+            DispatchQueue.main.async {
+                self.deliverLegacyNotification(notification)
+            }
+        }
+    }
+
+    private func deliverLegacyNotification(_ notification: NSUserNotification) {
+        NSUserNotificationCenter.default.removeAllDeliveredNotifications()
+        NSUserNotificationCenter.default.deliver(notification)
+
+        // remove after userset number of seconds if not taken action
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(preferences.notificationsLength)) {
+            NSUserNotificationCenter.default.removeDeliveredNotification(notification)
+        }
+    }
+
+    /// Called by notification delegate
 	func handleAction() {
         spotifyInteractor.nextTrack()
 	}
-    
-    private func progress(for track: Track?) -> String {
-        guard
-            let position = spotifyInteractor.playerPosition,
-            let duration = track?.duration else {
-                return "00:00/00:00"
-        }
-        
-        let percentage = position / (Double(duration) / 1000.0)
-        
-        let progressDone = "▪︎"
-        let progressNotDone = "⁃"
-        let progressMax = 14
-        let currentProgress = Int(Double(progressMax) * percentage)
-        
-        let progressString = String(repeating: progressDone, count: currentProgress) + String(repeating: progressNotDone, count: progressMax - currentProgress)
-        
-        let now = convert(seconds: Int(position))
-        let length = convert(seconds: duration / 1000)
-		
-		let nowS = "\(now.minutes)".withLeadingZeroes + ":" + "\(now.seconds)".withLeadingZeroes
-		let lengthS = "\(length.minutes)".withLeadingZeroes + ":" + "\(length.seconds)".withLeadingZeroes
-		
-        return "\(nowS)  \(progressString)  \(lengthS)"
-    }
-    
-    private func convert(seconds: Int) -> (minutes: Int, seconds: Int) {
-        return ((seconds % 3600) / 60, (seconds % 3600) % 60)
-    }
 }
-
--- SpotifyNotify/Models/Notification.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Notification.swift
-//  SpotifyNotify
-//
-//  Created by Paul Williamson on 07/03/2019.
-//  Copyright © 2019 Szymon Maślanka. All rights reserved.
-//
-
-import Foundation
